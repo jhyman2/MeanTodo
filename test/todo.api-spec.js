da7a8274da25506318fe98a81d151efa812a352c
bandress <bandress@skylinenet.net>
2015-04-30 15:14:08 -0400

    added test for todo api update method

--- test/todo.api-spec.js
+++ test/todo.api-spec.js
@@ -1,11 +1,12 @@
-'use strict';
+; 'use strict';
 
-var mocha = require('mocha');
-var chai = require('chai');
-var sinon = require('sinon');
-var assert = chai.assert;
-var expect = chai.expect;
-var rewire = require('rewire');
+var mocha = require('mocha'),
+    sinon = require('sinon'),
+    chai = require('chai'),
+    rewire = require('rewire'),
+    assert = chai.assert,
+    expect = chai.expect,
+    should = chai.should();
 
 describe('API', function () {
   // Runs at the very beginning before any test has been run
@@ -32,4 +33,78 @@ describe('API', function () {
   after(function (done) {
     done();
   });
+});
+
+describe('API :: Todo', function () {
+  var todoApi = rewire('../todo.api'),
+      TodoMock;
+
+  beforeEach(function(done){
+    TodoMock = {
+      find: function(){},
+      create: function(){},
+      save: function(){},
+      findOne: function(){}
+    };
+
+    sinon.spy(TodoMock, 'find');
+    sinon.spy(TodoMock, 'create');
+    sinon.spy(TodoMock, 'save');
+    sinon.spy(TodoMock, 'findOne');
+
+    todoApi.__set__('Todo', TodoMock);
+
+    done();
+  });
+
+  it('should update', function (done) {
+    var req = {
+          params: {
+            id: 1
+          },
+          body: {
+            text: 'test text'
+          }
+        };
+
+    todoApi.update(req);
+
+    assert.ok(TodoMock.findOne.called);
+    assert.typeOf(TodoMock.findOne.args[0][0], 'object');
+    TodoMock.findOne.args[0][0]._id.should.equal(1);
+    assert.typeOf(TodoMock.findOne.args[0][1], 'function');
+
+    done();
+  });
+
+  it('should fail update', function (done) {
+    var req = {
+          params: {
+            id: 0
+          },
+          body: {
+            text: 'test text'
+          }
+        },
+        res = {
+          status: function () {return this;},
+          send: function () {return this;}
+        },
+        next = sinon.spy;
+
+    sinon.spy(res, 'status');
+    sinon.spy(res, 'send');
+
+    todoApi.update(req, res, next);
+
+    assert.ok(res.status.called);
+    assert.ok(res.send.called);
+    assert.notOk(next.called);
+    assert.typeOf(res.status.args[0][0], 'number');
+    res.status.args[0][0].should.equal(400);
+    assert.typeOf(res.send.args[0][0], 'string');
+
+    done();
+  });
+
 });
