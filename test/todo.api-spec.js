92961bef0f05962680f36b22d768f59aab5c8408
Edwin Perez <edwinperez539@gmail.com>
2015-05-14 16:16:19 -0400

    Added google material design libs and new task goes to top.

--- test/todo.api-spec.js
+++ test/todo.api-spec.js
@@ -5,6 +5,7 @@ chai      = require('chai'),
 sinon     = require('sinon'),
 assert    = chai.assert,
 expect    = chai.expect,
+should    = chai.should(),
 rewire    = require('rewire'),
 todoApi   = rewire('../todo.api'),
 TodoMock;
@@ -19,12 +20,14 @@ describe('API :: Todo', function(){
     TodoMock = {
       find: function(){},
       create: function(){},
-      save: function(){}
+      save: function(){},
+      findOne: function (idObj, cb) {}
     };
 
     sinon.spy(TodoMock, 'find');
     sinon.spy(TodoMock, 'create');
     sinon.spy(TodoMock, 'save');
+    sinon.spy(TodoMock, 'findOne');
 
     todoApi.__set__('Todo', TodoMock);
 
@@ -45,17 +48,38 @@ describe('API :: Todo', function(){
     done();
   });
 
-  it('should create', function(done){
-    var req = { body: { text: 'Test Todo' } },
+  it('should update good data', function (done) {
+    var req = { params: { id: 1 }, body: { text: 'test text'}},
     res     = sinon.spy,
     next    = sinon.spy;
 
-    todoApi.create(req, res, next);
+    todoApi.update(req, res, next);
 
-    // assert.ok(TodoMock.save.called);
-    // assert.ok(TodoMock.save.called);
-    // assert.typeOf(TodoMock.save.args[0][0], 'object');
-    // assert.typeOf(TodoMock.save.args[0][1], 'function');
+    assert.ok(TodoMock.findOne.called);
+    assert.typeOf(TodoMock.findOne.args[0][0], 'object');
+    TodoMock.findOne.args[0][0]._id.should.equal(1);
+    assert.typeOf(TodoMock.findOne.args[0][1], 'function');
+
+    done();
+  });
+
+  it('should fail update with bad data', function (done) {
+    var req = { params: { id: 0 }, body: { text: 'test text'}},
+    res     = sinon.spy,
+    next    = sinon.spy;
+
+    res.status = function (code) {
+      code.should.equal(400);
+      return res;
+    };
+
+    res.send = function (message) {
+      message.should.be.a('string');
+      return res;
+    }
+
+    todoApi.update(req, res, next);
+    assert.ok(!TodoMock.findOne.called);
 
     done();
   });
